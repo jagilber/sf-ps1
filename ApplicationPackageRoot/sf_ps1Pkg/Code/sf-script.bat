df0e01849930f6a5637294bab333bd55a39cbb39
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-12 10:13:33 -0500

    init

---
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
@@ -0,0 +1,3 @@
+﻿%cd%
+set
+powershell.exe -nologo -noprofile -noninteractive -windowstyle hidden -file %cd%\..\%Fabric_ServicePackageName%.%Fabric_CodePackageName%.1.0.0\sf-script.ps1
---
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.ps1
@@ -0,0 +1,52 @@
+###########################
+# TCP connection stats
+###########################
+param(
+	$sleepMinutes = 1,
+    $nodeName = $env:Fabric_NodeName
+)
+
+while ($true) {
+    $netStat = @{ }
+    if(!get-serviceFabricCluster)
+    {
+        Connect-ServiceFabricCluster
+    }
+
+	$netStatRaw = Get-NetTCPConnection 
+    $netStatSelected = $netStatRaw | select LocalPort, State
+	$netStatSelected | group State | % { $netStat["$($_.Name)_Conn"] = $_.Count }
+	$netStatSelected | sort State, LocalPort | select -Unique State, LocalPort | group State | % { $netStat["$($_.Name)_Ports"] = $_.Count }
+	$netStatObj = $netStat.GetEnumerator() | sort name | % -Begin { [PSObject]$o = @{ } } { $o | Add-Member -NotePropertyName $_.Name -NotePropertyValue $_.Value } -End { $o }
+
+	$msg = "$(get-date) all:`r`n$($netStatObj | ConvertTo-Json)`r`n"
+	$netStatFG = $netStatRaw | where-object OwningProcess -eq (get-process fabricgateway).id
+    $netSTatFGGrouped = $netStatFG | group state
+	$msg += "$(get-date) fabricgateway:`r`n$($netStatFGGrouped | out-string)`r`n"
+    write-host $msg
+    
+    $level = 'Ok'
+    if($netStatFG.count > 900)
+    {
+        $level = 'Ok'
+    }
+    elseif($netStatFG.Count -eq 1000)
+    {
+        $level = 'Error'
+    }
+
+
+	$error.clear()
+	write-host "Send-ServiceFabricNodeHealthReport -NodeName $nodeName -HealthState $level -SourceId 'NodeErrata' -HealthProperty 'NetStat' -Description $msg"
+	$result = Send-ServiceFabricNodeHealthReport -NodeName $nodeName -HealthState $level -SourceId 'NodeErrata' -HealthProperty 'NetStat' -Description $msg
+    if($error -or $result) 
+    { 
+        Write-host ($result | out-string)
+        Write-host ($error | out-string)
+        Write-Error ($error | out-string)
+        $error.Clear()
+    }
+
+	Write-Host "Sleeping for $sleepMinutes minutes"
+	Start-Sleep -Seconds ($sleepMinutes * 60)
+}
