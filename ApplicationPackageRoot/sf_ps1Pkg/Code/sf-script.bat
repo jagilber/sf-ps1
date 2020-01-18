3834babf4d3c68fcbf26bfd10ba388fc60af39b5
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-18 08:34:26 -0500

    add codeVersion as env var

--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
@@ -2,9 +2,10 @@
 set
 %1
 %scripts%
-set codeVersion=1.0.0
+%codeVersion%
+rem set codeVersion=1.0.0
 
 cd ..\%Fabric_ServicePackageName%.%Fabric_CodePackageName%.%codeVersion%
 set scriptFile=%cd%\sf-script-manager.ps1 -scripts
-powershell.exe -nologo -noprofile -noninteractive -windowstyle hidden -file %scriptFile% %scripts%
-rem start powershell.exe -nologo -noprofile -file %scriptFile% %scripts%
+rem powershell.exe -nologo -noprofile -noninteractive -windowstyle hidden -file %scriptFile% %scripts%
+powershell.exe -nologo -noprofile -file %scriptFile% %scripts%
