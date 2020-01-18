f4835276918434587e053e6eaaeeff28cd9ae7f0
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-18 09:30:37 -0500

    test multi script and job cleanup

--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script-manager.ps1
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script-manager.ps1
@@ -11,7 +11,7 @@ $global:success = 0
 
 function main() {
     try {
-        write-host "starting"
+        write-log "starting"
         remove-jobs
         start-jobs
         monitor-jobs
@@ -21,18 +21,18 @@ function main() {
     }
     finally {
         remove-jobs
-        write-host "finished"
+        write-log "finished"
     }
 }
 
 function monitor-jobs() {
-    write-host "monitoring jobs"
+    write-log "monitoring jobs"
     while (get-job) {
         foreach ($job in get-job) {
-            write-verbose ($job | fl * | out-string)
+            write-log ($job | fl * | out-string)
 
-            if ($job.state -ine "running") {
-                write-host ($job | fl * | out-string)
+            if ($job.state -ieq "completed") { # -ine "running") {
+                write-log ($job | fl * | out-string)
                 if ($job.state -imatch "fail" -or $job.statusmessage -imatch "fail") {
                     [void]$global:joboutputs.add(($global:jobs[$job.id]), ($job | ConvertTo-Json))
                     $global:fail++
@@ -41,7 +41,7 @@ function monitor-jobs() {
                     [void]$global:joboutputs.add(($global:jobs[$job.id]), ($job.output | ConvertTo-Json))
                     $global:success++
                 }
-                write-host ($job.output | ConvertTo-Json)
+                write-log ($job.output | ConvertTo-Json)
                 $job.output
                 Remove-Job -Id $job.Id -Force  
             }
@@ -49,7 +49,7 @@ function monitor-jobs() {
                 $jobInfo = Receive-Job -Job $job
                 
                 if ($jobInfo) {
-                    write-host ($jobInfo | fl * | out-string)
+                    write-log ($jobInfo | fl * | out-string)
                 }
             }
             Start-Sleep -Seconds 1
@@ -58,50 +58,64 @@ function monitor-jobs() {
 }
 
 function remove-jobs() {
-    write-host "removing jobs"
+    write-log "removing jobs"
     try {
         if (@(get-job).Count -gt 0) {
             foreach ($job in get-job) {
-                write-host "removing job $($job.Name)"
+                write-log "removing job $($job.Name)"
                 $job.StopJob()
                 Remove-Job $job -Force
             }
         }
     }
     catch {
-        write-host $Error
+        write-log "error:$($Error | out-string)"
         $error.Clear()
     }
 }
 
 function start-jobs() {
-    write-host "start jobs"
-    foreach ($script in $scripts) {
+    write-log "start jobs"
+    foreach ($script in @($scripts)) {
         $argIndex = $script.LastIndexOf('.ps1') + 4
         $scriptFile = $script.substring(0, $argIndex)
-        $scriptFileName = [io.path]::GetFileName($scriptFile)
         $scriptArgs = $script.substring($argIndex)
-        write-host "checking file:$scriptFile`r`n`targs:$scriptsArgs"
+        $scriptFileName = [io.path]::GetFileName($scriptFile)
+        write-log "checking file:$scriptFileName`r`n`targs:$scriptArgs"
 
         if($scriptFile.tolower().startswith("http")) {
-            write-host "downloading $scriptFile"
+            write-log "downloading $scriptFile"
             $downloadedFile = "$env:temp\$scriptFileName"
             (new-object net.webclient).DownloadFile($scriptFile, $downloadedFile)
             $scriptFile = $downloadedFile
         }
         elseif(!(test-path $scriptFile)) {
-            write-error "$scriptFile does not exist"
+            write-log "error:$scriptFile does not exist"
             continue
         }
 
         $scriptFile = resolve-path $scriptFile
-        write-host "starting $scriptFile $scriptArgs"
-        start-job -Name $scriptFileName -ArgumentList @($scriptFile, $scriptArgs) -scriptblock { 
+
+        write-log "starting $scriptFile $scriptArgs"
+        start-job -Name $scriptFile -ArgumentList @($scriptFile, $scriptArgs) -scriptblock { 
             param($script,$args)
-            write-output "$script $args"
-            write-output (invoke-expression -command "$script $args")
+            write-log "$script $args"
+            write-log (invoke-expression -command "$script $args")
         }
     }
+}
+
+function write-log($data, $level) {
+    $data = "$(get-date):$data`r`n"
+
+    if($level -imatch "error") {
+        write-error $data
+    }
+    elseif($level -imatch "warning") {
+        Write-Warning $data
+    }
+
+    write-host $data
 }
 
 # execute script
--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
@@ -6,6 +6,6 @@ set
 rem set codeVersion=1.0.0
 
 cd ..\%Fabric_ServicePackageName%.%Fabric_CodePackageName%.%codeVersion%
-set scriptFile=%cd%\sf-script-manager.ps1 -scripts
+set scriptFile=%cd%\sf-script-manager.ps1
 rem powershell.exe -nologo -noprofile -noninteractive -windowstyle hidden -file %scriptFile% %scripts%
-powershell.exe -nologo -noprofile -file %scriptFile% %scripts%
+powershell.exe -nologo -noprofile -file "%scriptFile%" -scripts "%scripts%"
