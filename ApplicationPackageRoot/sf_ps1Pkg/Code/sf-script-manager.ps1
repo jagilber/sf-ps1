fffcb610d320fc69258f3e3a513f28ebaf715a83
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-21 07:53:10 -0500

    sleepSeconds

--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script-manager.ps1
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script-manager.ps1
@@ -1,10 +1,10 @@
 #
 [cmdletbinding()]
 param(
-    [string]$scripts = "",
-    [int]$sleepMinutes = 1,
+    [string]$scripts = $env:scripts,
+    [int]$sleepSeconds = ($env:sleepSeconds, 1 -ne $null)[0],
     [string]$detail = $env:detail,
-    [int]$timeToLiveMinutes = 60
+    [int]$timeToLiveMinutes = ($env:timeToLiveMinutes, 60 -ne $null)[0]
 )
 
 $error.Clear()
@@ -57,7 +57,7 @@ function monitor-jobs() {
                     write-log -data $jobInfo -report $job.name
                 }
             }
-            start-sleep -Seconds ($sleepMinutes * 60)
+            start-sleep -Seconds $sleepSeconds
         }
     }
 }
