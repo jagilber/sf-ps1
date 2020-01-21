4206424478ddfa8ad7340f8f9b384f8ee12896e9
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-21 08:28:33 -0500

    cleanup

--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-script.bat
@@ -8,5 +8,4 @@ rem set codeVersion=1.0.0
 whoami
 cd ..\%Fabric_ServicePackageName%.%Fabric_CodePackageName%.%codeVersion%
 set scriptFile=%cd%\sf-script-manager.ps1
-rem powershell.exe -nologo -noprofile -noninteractive -windowstyle hidden -file %scriptFile% %scripts%
-powershell.exe -nologo -noprofile -file "%scriptFile%" -scripts "%scripts%"
+powershell.exe -nologo -noprofile -file "%scriptFile%"
