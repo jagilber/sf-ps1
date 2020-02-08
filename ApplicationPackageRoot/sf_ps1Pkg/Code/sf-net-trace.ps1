5bd62f08d82f7f92068cd38f3c4b81386ef6ad8b
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-02-07 22:33:41 -0500

    cleanup

--- ApplicationPackageRoot/sf_ps1Pkg/Code/sf-net-trace.ps1
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/sf-net-trace.ps1
@@ -8,8 +8,7 @@ param(
     [string]$myErrorDescription = ($env:myErrorDescription, "capture network trace with netsh" -ne $null)[0],
     [string]$myDescription = ($env:myDescription, "capture network trace with netsh" -ne $null)[0],
     [string]$traceFile = ($env:traceFile, "$pwd\net.etl" -ne $null)[0],
-    [int]$maxSizeMb = ($env:maxSize, 1024 -ne $null)[0],
-    [string]$csvFile = ($env:csvFile, "$pwd\net.csv" -ne $null)[0]
+    [int]$maxSizeMb = ($env:maxSize, 1024 -ne $null)[0]
 )
 
 $ErrorActionPreference = "continue"
@@ -17,10 +16,8 @@ $error.clear()
 
 function main() {
     try{
-        $session = "nettrace"        
         $timer = get-date
-        write-output "$($MyInvocation.ScriptName)`r`n$psboundparameters : $myDescription`r`n"
-
+        write-host "$($MyInvocation.ScriptName)`r`n$psboundparameters : $myDescription`r`n"
         $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 
         if(!$isAdmin){
@@ -28,34 +25,32 @@ function main() {
             return
         }
 
-        write-output "$(get-date) stopping existing trace`r`n"
-        write-output (netsh trace stop)
+        write-host "$(get-date) stopping existing trace`r`n" -ForegroundColor green
+        netsh trace stop
 
-        write-output "$(get-date) starting trace`r`n"
-        write-output (netsh trace start capture=yes overwrite=yes maxsize=$maxSizeMb tracefile=$traceFile filemode=circular)
+        write-host "$(get-date) starting trace" -ForegroundColor green
+        netsh trace start capture=yes overwrite=yes maxsize=$maxSizeMb tracefile=$traceFile filemode=circular
 
-        write-output "$(get-date) sleeping $sleepMinutes minutes`r`n"
+        write-host "$(get-date) sleeping $sleepMinutes minutes" -ForegroundColor green
         start-sleep -Seconds ($sleepMinutes * 60)
 
-        write-output "$(get-date) stopping trace`r`n"
-        write-output (netsh trace stop)
+        write-host "$(get-date) stopping trace" -ForegroundColor green
+        netsh trace stop
 
         if ($error) {
-            write-output "ERROR:$myErrorDescription`r`n"
-            write-output "$($error | out-string)`r`n"
+            write-host "ERROR:$myErrorDescription`r`n"
+            write-host "$($error | out-string)`r`n"
         }
 
-        write-output "copying files`r`n"
+        write-host "$(get-date) copying files" -ForegroundColor green
         copy net.* ..\log
-        
-        write-output "$(get-date) timer: $(((get-date) - $timer).tostring())`r`n"
-        write-output "$(get-date) finished`r`n"
-        
+
+        write-host "$(get-date) timer: $(((get-date) - $timer).tostring())"
+        write-host "$(get-date) finished" -ForegroundColor green
     }
     catch {
-        write-output ($_ | out-string)
-        write-output ($error | out-string)
-        
+        write-error ($_ | out-string)
+        write-error ($error | out-string)
 	}
 }
 
