87308c54f3422c6fc2b69c22458b9cc69a9e3544
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-02-26 08:26:07 -0500

    httplistener updates

--- ApplicationPackageRoot/sf_ps1Pkg/Code/test-http-listener.ps1
+++ ApplicationPackageRoot/sf_ps1Pkg/Code/test-http-listener.ps1
@@ -5,7 +5,7 @@ param(
     [int]$port = 80,
     [int]$count = 0,
     [string]$hostName = 'localhost',
-    [switch]$isClient,
+    [switch]$server,
     [hashtable]$clientHeaders = @{ },
     [string]$clientBody = 'test message from client',
     [ValidateSet('GET', 'POST', 'HEAD')]
@@ -19,20 +19,21 @@ $scriptParams = $PSBoundParameters
 
 function main() {
     try {
-        if ($isClient) {
+        if (!$server) {
             start-client
         }
         else {
-            # start client so server can periodically resume without async
-            if ($host.Name -ine "ServerRemoteHost") {
+            # start as job so server can exit gracefully after 2 minutes of cancellation
+            #if ($host.Name -ine "ServerRemoteHost") {
+            if ($false) {
                 # called on foreground thread only
-                start-job -ScriptBlock { param($script,$params); . $script @params } -ArgumentList $MyInvocation.ScriptName, $scriptParams
+                start-job -ScriptBlock { param($script, $params); . $script @params } -ArgumentList $MyInvocation.ScriptName, $scriptParams
 
-                while(get-job) {
-                    foreach($job in get-job) {
+                while (get-job) {
+                    foreach ($job in get-job) {
                         $jobInfo = Receive-Job -Job $job | convertto-json -Depth 5
-                        if($jobInfo) { write-host $jobInfo }
-                        if($job.State -ine "running") {
+                        if ($jobInfo) { write-host $jobInfo }
+                        if ($job.State -ine "running") {
                             Remove-Job -Job $job -Force
                         }
                     }
@@ -61,41 +62,47 @@ function start-client([hashtable]$header = $clientHeaders, [string]$body = $clie
     $iteration = 0
 
     while ($iteration -lt $count -or $count -eq 0) {
-        $requestId = [guid]::NewGuid().ToString()
-        write-verbose "request id: $requestId"
-        if ($header.Count -lt 1) {
-            $header = @{
-                'accept'                 = 'application/json'
-                #'authorization'          = "Bearer $(Token)"
-                'content-type'           = 'text/html' #'application/json'
-                'host'                   = $hostName
-                'x-ms-app'               = [io.path]::GetFileNameWithoutExtension($MyInvocation.ScriptName)
-                'x-ms-user'              = $env:USERNAME
-                'x-ms-client-request-id' = $requestId
-            } 
+        try {
+            $requestId = [guid]::NewGuid().ToString()
+            write-verbose "request id: $requestId"
+            if ($header.Count -lt 1) {
+                $header = @{
+                    'accept'                 = 'application/json'
+                    #'authorization'          = "Bearer $(Token)"
+                    'content-type'           = 'text/html' #'application/json'
+                    'client'                 = $env:COMPUTERNAME
+                    'host'                   = $hostName
+                    'x-ms-app'               = [io.path]::GetFileNameWithoutExtension($MyInvocation.ScriptName)
+                    'x-ms-user'              = $env:USERNAME
+                    'x-ms-client-request-id' = $requestId
+                } 
+            }
+
+            $params = @{
+                method  = $method
+                uri     = $uri
+                headers = $header
+            }
+        
+            if ($method -ieq 'POST' -and ![string]::IsNullOrEmpty($body)) {
+                $params += @{body = $body }
+            }
+            write-verbose ($header | convertto-json)
+            Write-Verbose ($params | fl * | out-string)
+    
+            $error.clear()
+            $result = Invoke-WebRequest -verbose @params
+            write-host $result 
+        
+            if ($error) {
+                write-host "$($error | out-string)"
+                $error.Clear()
+            }
+        }
+        catch {
+            Write-Warning "exception reading from server`r`n$($_)"
         }
 
-        $params = @{
-            method  = $method
-            uri     = $uri
-            headers = $header
-        }
-        
-        if ($method -ieq 'POST' -and ![string]::IsNullOrEmpty($body)) {
-            $params += @{body = $body }
-        }
-        write-verbose ($header | convertto-json)
-        Write-Verbose ($params | fl * | out-string)
-    
-        $error.clear()
-        $result = Invoke-WebRequest -verbose @params
-        write-host $result
-        
-        if ($error) {
-            write-host "$($error | out-string)"
-            $error.Clear()
-        }
-    
         start-sleep -Seconds 1
         $iteration++
     }
@@ -115,48 +122,65 @@ function start-server() {
     }
 
     while ($iteration -lt $count -or $count -eq 0) {
-        $context = $http.GetContext()
-        [string]$html = $null
-        write-host "$(get-date) http server $($context.Request.UserHostAddress) received $($context.Request.HttpMethod) request:`r`n"
+        try {
+            $context = $http.GetContext()
+            [hashtable]$requestHeaders = @{ }
+            [string]$requestHeadersString = ""
 
-        if ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/') {
-            write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
-            $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
-            $html += $context | ConvertTo-Json -depth 99
-        }
-        elseif ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/min') {
-            write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
-            $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
-        }
-        elseif ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq $absolutePath) {
-            write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
-            $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
-            $html += $context | ConvertTo-Json -depth 99
-        }
-        elseif ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/') {
-            $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
-            [byte[]]$inputBuffer = @(0) * $maxBuffer
-            $context.Request.InputStream.Read($inputBuffer, 0, $maxBuffer)# $context.Request.InputStream.Length)
-            $html += "INPUT STREAM: $(([text.encoding]::ASCII.GetString($inputBuffer)).Trim())`r`n"
-            $html += $context | ConvertTo-Json -depth 99
-        }
-        else {
-            #$html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
-        }
+            foreach ($header in $context.Request.Headers.AllKeys) {
+                $requestHeaders.Add($header, @($context.Request.Headers.GetValues($header)))
+                $requestHeadersString += "$($header):$(($context.Request.Headers.GetValues($header)) -join ';'),"
+            }
 
-        if ($html) {
-            write-host $html
-            #respond to the request
-            $buffer = [Text.Encoding]::ASCII.GetBytes($html)
-            $context.Response.ContentLength64 = $buffer.Length
-            $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
-            $context.Response.OutputStream.Close()
-        }
-        else {
-            $context.Response.OutputStream.Close()
-        }
+            [string]$html = $null
+            write-host "$(get-date) http server $($context.Request.UserHostAddress) received $($context.Request.HttpMethod) request:`r`n"
+
+            if ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/') {
+                write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
+                $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
+                $html += "`r`nREQUEST HEADERS:`r`n$($requestHeaders | out-string)`r`n"
+                $html += $context | ConvertTo-Json -depth 99
+            }
+            elseif ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/min') {
+                write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
+                $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
+                $html += "`r`nREQUEST HEADERS:`r`n$($requestHeaders | out-string)`r`n"
+            }
+            elseif ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq $absolutePath) {
+                write-host "$(get-date) $($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -ForegroundColor Magenta
+                $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
+                $html += $context | ConvertTo-Json -depth 99
+            }
+            elseif ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/') {
+                $html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
+                [byte[]]$inputBuffer = @(0) * $maxBuffer
+                $context.Request.InputStream.Read($inputBuffer, 0, $maxBuffer)# $context.Request.InputStream.Length)
+                $html += "INPUT STREAM: $(([text.encoding]::ASCII.GetString($inputBuffer)).Trim())`r`n"
+                $html += $context | ConvertTo-Json -depth 99
+            }
+            else {
+                #$html = "$(get-date) http server $($env:computername) received $($context.Request.HttpMethod) request:`r`n"
+            }
+
+            if ($html) {
+                write-host $html
+                #respond to the request
+                $buffer = [Text.Encoding]::ASCII.GetBytes($html)
+                $context.Response.ContentLength64 = $buffer.Length
+                $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+                $context.Response.OutputStream.Close()
+            }
+            else {
+                # head
+                $context.Response.Headers.Add("requestHeaders", $requestHeadersString)    
+                $context.Response.OutputStream.Close()
+            }
         
-        $iteration++
+            $iteration++
+        }
+        catch {
+            Write-Warning "error $($_)"
+        }
     }
 }
 
